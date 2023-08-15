7ad7e0488cf5013a34d146386f5f24cf202ae156
Rodrigo Yañez <122339965+TheRebelPath@users.noreply.github.com>
2023-08-14 23:50:04 -0600

    Update ExploreCard.jsx

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -35,7 +35,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         glassmorphism mb-[16px]`}
         >
           <img
-            src="/headset.svg"
+            src="./headset.svg"
             alt="headset"
             className="w-1/2 h-1/2 object-contain"
           />
